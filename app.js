ad48b0c6fcbd442ccb2880977e2b14ef1eb6dd30
swagata <swagata.kundu@daffodilsw.com>
2017-06-08 11:05:20 +0530

    Add images

--- app.js
+++ app.js
@@ -6,26 +6,27 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.locals.basedir = app.get('views');
 
+var static = require('./assets/data/static');
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', function(req, res) {
-    res.render('home', { title: 'Hey', message: 'Hello there!' })
+    res.render('home', { data: static })
 });
 app.get('/work', function(req, res) {
-    res.render('work', { title: 'Hey', message: 'Hello there!' })
+    res.render('work', { data: static })
 });
 
 app.get('/about', function(req, res) {
-    res.render('about', { title: 'Hey', message: 'Hello there!' })
+    res.render('about', { data: static })
 });
 
 app.get('/contact', function(req, res) {
-    res.render('contact', { title: 'Hey', message: 'Hello there!' })
+    res.render('contact', { data: static })
 });
 
 app.use(function(req, res, next) {
-    res.render('404', { status: 404, url: req.url });
+    res.render('404', { data: static });
 });
 
 

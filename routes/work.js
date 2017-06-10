08c3452f9db3b98d32599e778569d86f3cb6120e
swagata <swagata.kundu@daffodilsw.com>
2017-06-10 10:23:44 +0530

    changes

--- routes/work.js
+++ routes/work.js
@@ -5,11 +5,31 @@ const metaInfo = {
     description: 'Skilled in Node.js, Bootstrap, AngularJS, JavaScript, and MongoDB'
 }
 
+const work = [{
+        name: 'Night Out',
+        desc: `My Night out is a business concept for various cities. 
+        Here business owner register their business and public user can find them as useful. 
+        This business are mainly of Restaurant, Hotel, Bar types.
+         Business owners have facility to promote their business and organize various events at their place by letting know the customer via this media. This project has mobile application development and as well as website. There are multiple level of business in this model. Site owner make lease of various city website to local licensee. Later licensee sales spot on the site to business owners. 
+        There are various notification scenarios via mail as well as push notification at realtime like reservation reminder, new event reminder, business expiry reminder etc to make the users always updated.`,
+        tech: 'Node JS, Angular Js, MySQL, Bootstrap',
+        duration: 'April 2016 - Prasent',
+        role: `Designed database and created API for mobile APP`,
+        images: []
+    }
+
+]
+
 module.exports = (req, res, next) => {
-    res.render('work', { data: static, metainfo: metaInfo }, function(err, html) {
-        if (err) {
-            return next(err);
-        }
-        return res.send(html);
-    });
+    res
+        .render('work', {
+            data: static,
+            metainfo: metaInfo,
+            work: work
+        }, (err, html) => {
+            if (err) {
+                return next(err);
+            }
+            return res.send(html);
+        });
 };

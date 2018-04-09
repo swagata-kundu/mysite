792d32eeb9dbf2e3beb857b7363193b97c8427aa
swagata-kundu <me@swagatak.in>
2018-04-09 22:55:47 +0530

    new config

--- app.js
+++ app.js
@@ -4,38 +4,39 @@ const Config = require('config');
 const Compression = require('compression');
 
 
-var App = Express();
+const App = Express();
 
 
 App.set('view engine', 'pug');
-App.set('views', __dirname + '/views');
+App.set('views', `${__dirname }/views`);
 App.locals.basedir = App.get('views');
 
 
-App.use('/assets', Compression(),
-    Express.static(Path.join(__dirname, 'assets')));
+App.use(
+  '/assets', Compression(),
+  Express.static(Path.join(__dirname, 'assets')),
+);
 
 
 App.use(require('./routes'));
 
 App.use((req, res, next) => {
-    res.render('404', {
-        data: static
-    });
+  res.render('404', {
+  });
 });
 
 
 App.use(require('connect-livereload')({
-    port: 6300,
-    ignore: ['.js']
+  port: 6300,
+  ignore: ['.js'],
 }));
 
 
 App.listen(Config.get('port'), () => {
-    App.emit('online');
+  App.emit('online');
 });
 
 // print when online
 App.on('online', () => {
-    console.info('Application is listening on port ', Config.get('port'));
+  console.info('Application is listening on port ', Config.get('port'));
 });
